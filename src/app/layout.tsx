a1018a4a8bd8a8e7385b2b597ab02b0df6a34e42
Steve <contact@stevedylan.dev>
2025-10-30 16:36:15 -0400

    chore: added telegram banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Provider } from "./provider";
+import { Banner } from "fumadocs-ui/components/banner";
+import Link from "fumadocs-core/link";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -87,6 +89,31 @@ export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
 			<body className="flex flex-col min-h-screen">
+				<Banner
+					id="telegram"
+					variant="rainbow"
+					rainbowColors={["#4F57FA", "#2F99FF"]}
+					className="gap-2"
+				>
+					<p>Join our community of builders on</p>
+					<Link
+						className="underline inline-flex items-center gap-1"
+						href="https://t.me/openzeppelin_tg"
+					>
+						Telegram!
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							viewBox="0 0 24 24"
+						>
+							<title>Telegram</title>
+							<path
+								fill="currentColor"
+								d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19c-.14.75-.42 1-.68 1.03c-.58.05-1.02-.38-1.58-.75c-.88-.58-1.38-.94-2.23-1.5c-.99-.65-.35-1.01.22-1.59c.15-.15 2.71-2.48 2.76-2.69a.2.2 0 0 0-.05-.18c-.06-.05-.14-.03-.21-.02c-.09.02-1.49.95-4.22 2.79c-.4.27-.76.41-1.08.4c-.36-.01-1.04-.2-1.55-.37c-.63-.2-1.12-.31-1.08-.66c.02-.18.27-.36.74-.55c2.92-1.27 4.86-2.11 5.83-2.51c2.78-1.16 3.35-1.36 3.73-1.36c.08 0 .27.02.39.12c.1.08.13.19.14.27c-.01.06.01.24 0 .38"
+							/>
+						</svg>
+					</Link>
+				</Banner>
 				<GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID as string} />
 				<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID as string} />
 				<Provider>{children}</Provider>
