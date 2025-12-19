8b4bfe6e999b313ddbe7cf71e1b9c904930917a2
immrsd <immrsd.eth@gmail.com>
2025-12-19 21:54:20 +0300

    Update version in links from 2.x to 3.x

--- content/contracts-cairo/3.x/utils/replacements.ts
+++ content/contracts-cairo/3.x/utils/replacements.ts
@@ -1,7 +1,7 @@
 import { CLASS_HASHES, CLASS_HASH_SCARB_VERSION, UMBRELLA_VERSION } from "./constants";
 
 export const REPLACEMENTS = {
-  include: ['**/content/contracts-cairo/2.x/**/*.mdx'],
+  include: ['**/content/contracts-cairo/3.x/**/*.mdx'],
   replacements: {
     umbrella_version: UMBRELLA_VERSION,
     class_hash_scarb_version: CLASS_HASH_SCARB_VERSION,
